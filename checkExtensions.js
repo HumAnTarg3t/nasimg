1245f9508ade48763adb12d525b2827522ff9918
niktsa <nikita-98@hotmail.com>
2024-12-28 16:16:41 +0100

    format

--- checkExtensions.js
+++ checkExtensions.js
@@ -1,50 +1,47 @@
-    var fs = require('fs');
-    require('dotenv').config()
-    const path = require('path');
-    const original_file_path = process.env.original_file_path
+var fs = require("fs");
+require("dotenv").config();
+const path = require("path");
+const original_file_path = process.env.original_file_path;
 
-    let files = fs.readdirSync(original_file_path, { recursive: true });
-    let extensionArray= []
+let files = fs.readdirSync(original_file_path, { recursive: true });
+let extensionArray = [];
 
-    /**
-     * An object to store the count of files for each extension.
-     * 
-     * Keys are file extensions (e.g., 'txt', 'mp4'), and values are the counts of files with that extension.
-     * 
-     * @type {Object<string, number>}
-     */
-    let extensionCounters = {}; // Object to store counters for each extension
+/**
+ * An object to store the count of files for each extension.
+ *
+ * Keys are file extensions (e.g., 'txt', 'mp4'), and values are the counts of files with that extension.
+ *
+ * @type {Object<string, number>}
+ */
+let extensionCounters = {}; // Object to store counters for each extension
 
-    function isFile(pathItem) {
-        return !!path.extname(pathItem);
+function isFile(pathItem) {
+  return !!path.extname(pathItem);
+}
+
+function checkExtensions() {
+  files.forEach((e) => {
+    // Split the item name to get the extension
+    let extension = e.split(".");
+    let lastElemInArray = extension[extension.length - 1];
+
+    if (isFile(e)) {
+      // Check if the extension is not already in the array and the item has an extension
+      if (!extensionArray.includes(lastElemInArray) && extension.length > 1) {
+        extensionArray.push(lastElemInArray);
+        extensionCounters[lastElemInArray] = 0; // Initialize counter for the extension
+      }
+
+      // Increment the counter for the extension
+      if (extensionCounters[lastElemInArray] !== undefined) {
+        extensionCounters[lastElemInArray]++;
+      }
     }
+  });
+}
+checkExtensions();
 
-    function checkExtensions() {
-        files.forEach(e => {
+let entries = Object.entries(extensionCounters);
+let sorted = entries.sort((a, b) => b[1] - a[1]);
 
-            // Split the item name to get the extension
-            let extension = e.split(".");
-            let lastElemInArray = extension[extension.length - 1 ]
-             
-            if (isFile(e)) {
-                
-                // Check if the extension is not already in the array and the item has an extension
-                if (!extensionArray.includes(lastElemInArray) && extension.length > 1) {
-                    extensionArray.push(lastElemInArray);
-                    extensionCounters[lastElemInArray] = 0; // Initialize counter for the extension
-                }
-                
-                // Increment the counter for the extension
-                if (extensionCounters[lastElemInArray] !== undefined) {
-                    extensionCounters[lastElemInArray]++;
-                }
-            }
-        });
-        
-    }
-    checkExtensions()
-
-    let entries = Object.entries(extensionCounters);
-    let sorted = entries.sort((a, b) => b[1] - a[1]);
-
-    module.exports = sorted;
+module.exports = sorted;
--- main.js
+++ main.js
@@ -1,91 +1,89 @@
-var fs = require('fs');
-require('dotenv').config()
-const path = require('path');
-const original_file_path = process.env.original_file_path
-const new_file_path = process.env.new_file_path
+var fs = require("fs");
+require("dotenv").config();
+const path = require("path");
+const original_file_path = process.env.original_file_path;
+const new_file_path = process.env.new_file_path;
 let files = fs.readdirSync(original_file_path, { recursive: true });
-let folderNameArray = []
+let folderNameArray = [];
 
 function isFile(pathItem) {
-    return !!path.extname(pathItem);
+  return !!path.extname(pathItem);
 }
-
 
 //get folder name into an arrary
 async function getFolderNamesToArray() {
-    files.forEach(e =>{
-        try {
-            // Get file stats synchronously
-            const stats = fs.statSync(`${original_file_path}/${e}`);
-            let modifiedDate = stats.mtime;
-            modifiedDate = modifiedDate.toISOString().split('T')
-            let formatedDate = modifiedDate[0]
-            if (!folderNameArray.includes(formatedDate)) {
-                folderNameArray.push(formatedDate)
-            }
-        } catch (error) {
-            console.log(error);
-            }
-        })
-        console.log(`${folderNameArray.length} folders will be created`);
-        
+  files.forEach((e) => {
+    try {
+      // Get file stats synchronously
+      const stats = fs.statSync(`${original_file_path}/${e}`);
+      let modifiedDate = stats.mtime;
+      modifiedDate = modifiedDate.toISOString().split("T");
+      let formatedDate = modifiedDate[0];
+      if (!folderNameArray.includes(formatedDate)) {
+        folderNameArray.push(formatedDate);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  console.log(`${folderNameArray.length} folders will be created`);
 }
 
 async function createFolders() {
-    //create folders
-    folderNameArray.forEach(e =>{
-        try {
-            fs.mkdir(`${new_file_path}/${e}`,(e)=>{
-                //If the folder does not exist
-                if (e == null) {
-                    // console.log('Mappe opprettet');
-                }
-            });             
-        } 
-        catch (error) {
-            console.log(error);
+  //create folders
+  folderNameArray.forEach((e) => {
+    try {
+      fs.mkdir(`${new_file_path}/${e}`, (e) => {
+        //If the folder does not exist
+        if (e == null) {
+          // console.log('Mappe opprettet');
         }
-    })
-    
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
 }
 // console.log(`${folderCreationCount} folder/s created`);
 async function moveFiles() {
-    files.forEach(e =>{
+  files.forEach((e) => {
     try {
-        //if its a file, move
-        if (isFile(e)) {
-            const fileArrayWithName = e.split('/') 
-            // if the array has more then 1 indexes, chose the last
-            const nameOffile = fileArrayWithName[fileArrayWithName.length -1] || fileArrayWithName[0]  
-            // console.log(nameOffile);
+      //if its a file, move
+      if (isFile(e)) {
+        const fileArrayWithName = e.split("/");
+        // if the array has more then 1 indexes, chose the last
+        const nameOffile =
+          fileArrayWithName[fileArrayWithName.length - 1] ||
+          fileArrayWithName[0];
+        // console.log(nameOffile);
 
-            const stats = fs.statSync(`${original_file_path}/${e}`);
-            let modifiedDate = stats.mtime;
-            modifiedDate = modifiedDate.toISOString().split('T')
-            let formatedDate = modifiedDate[0]
+        const stats = fs.statSync(`${original_file_path}/${e}`);
+        let modifiedDate = stats.mtime;
+        modifiedDate = modifiedDate.toISOString().split("T");
+        let formatedDate = modifiedDate[0];
 
-
-
-            // console.log(`${original_file_path}/${e}`);
-            fs.rename(`${original_file_path}/${e}`,`${new_file_path}/${formatedDate}/${nameOffile}`, (err) => {
-                if (err) throw err;
-                // console.log(`Renamed ${original_file_path}/${e}: ${original_file_path}/${formatedDate}/${nameOffile}`);
-              });
-            
-        }else{
-                //not an image!!
-            // console.log(e);
-            
-        }
+        // console.log(`${original_file_path}/${e}`);
+        fs.rename(
+          `${original_file_path}/${e}`,
+          `${new_file_path}/${formatedDate}/${nameOffile}`,
+          (err) => {
+            if (err) throw err;
+            // console.log(`Renamed ${original_file_path}/${e}: ${original_file_path}/${formatedDate}/${nameOffile}`);
+          }
+        );
+      } else {
+        //not an image!!
+        // console.log(e);
+      }
     } catch (error) {
-     console.log(error);
+      console.log(error);
     }
-    })
-}    
-    
-    async function start() {
-        await getFolderNamesToArray()
-        await createFolders()
-        await moveFiles()
-    }
-    start()
+  });
+}
+
+async function start() {
+  await getFolderNamesToArray();
+  await createFolders();
+  await moveFiles();
+}
+start();
