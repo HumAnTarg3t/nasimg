6e325487fc6f8d02567c09642adf8832a1c6b6ae
niktsa <nikita-98@hotmail.com>
2025-01-05 15:44:18 +0100

    la til logger(), opprettet foundationArray også

--- functions/foundation.js
+++ functions/foundation.js
@@ -1,15 +1,71 @@
 var fs = require("fs");
 require("dotenv").config({ path: "../.env" });
+const logger = require("../helpers/logger");
+var path = require("path");
+var scriptName = path.basename(__filename);
+const isFile = require("./isFile");
 const original_file_path = process.env.original_file_path;
+const extension = ["jpg", "jpeg", "png", "mp4", "heic", "mov"];
 
 /**
- * An array that stores the filenames.
+ * An array with objects. Stores filename and filepath.
+ *
+ * Allowed files: const extension = ["jpg", "jpeg", "png", "mp4"];
+ *
+ *
+ * @type {Array{Object<string, string>}}
+ *
+ * [{path: '//qwe/zxc/', fileName: 'picture.jpg'}]
+ */
+let foundationArray = [];
+
+/**
+ * An array that stores the filenames of all type of files.
  *
  * @type {Array.string}
  */
 let files = fs.readdirSync(original_file_path, { recursive: true });
 
-// console.log(typeof(files));
-// console.log(files);
+files.forEach((e) => {
+  //velg kun filer
+  if (isFile(e)) {
+    const eSPlit = e.split("/");
+    //Hvis filen har mappenavn i fileName
 
-module.exports = files;
+    const fileName = eSPlit[eSPlit.length - 1];
+    const extenionCheck = extension.includes(
+      fileName.toLowerCase().split(".")[1]
+    );
+    if (eSPlit.length > 1 && extenionCheck) {
+      eSPlit.pop();
+      let folderName = 1;
+      eSPlit.forEach((e) => {
+        folderName += e + "/";
+      });
+
+      let obj = {
+        path: `${original_file_path}/${folderName.substring(1)}`,
+        fileName: `${fileName}`,
+      };
+      foundationArray.push(obj);
+    } else {
+      if (extension.includes(e.toLowerCase().split(".")[1])) {
+        let obj = { path: `${original_file_path}/`, fileName: `${e}` };
+        foundationArray.push(obj);
+      }
+    }
+  }
+});
+logger(
+  "info",
+  `found ${files.length} items. Filenames of all type of files.`,
+  scriptName
+);
+logger(
+  "info",
+  `found ${foundationArray.length} objects. Filename and filepath.`,
+  scriptName
+);
+// console.log(foundationArray);
+exports.files = files;
+exports.foundationArray = foundationArray;
