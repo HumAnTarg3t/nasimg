af0fbc5cbf446adbf59036ce3d24ffbad47966cb
niktsa <nikita-98@hotmail.com>
2025-01-05 15:44:48 +0100

    byttet til logger() og foundationArray

--- functions/getFolderNamesToArray.js
+++ functions/getFolderNamesToArray.js
@@ -1,7 +1,10 @@
 var fs = require("fs");
 require("dotenv").config({ path: "../.env" });
-const original_file_path = process.env.original_file_path;
-const { foundationArray, files } = require("../functions/foundation");
+// const original_file_path = process.env.original_file_path;
+const logger = require("../helpers/logger");
+var path = require("path");
+var scriptName = path.basename(__filename);
+const { foundationArray, files } = require("./foundation");
 
 /**
  * An array that stores the names of folders that will be created.
@@ -12,10 +15,10 @@ let folderNameArray = [];
 
 //get folder name into an arrary
 async function getFolderNamesToArray() {
-  files.forEach((e) => {
+  foundationArray.forEach((e) => {
     try {
       // Get file stats synchronously
-      const stats = fs.statSync(`${original_file_path}/${e}`);
+      const stats = fs.statSync(`${e.path}/${e.fileName}`);
       let modifiedDate = stats.mtime;
       modifiedDate = modifiedDate.toISOString().split("T");
       let formatedDate = modifiedDate[0];
@@ -23,12 +26,19 @@ async function getFolderNamesToArray() {
         folderNameArray.push(formatedDate);
       }
     } catch (error) {
-      console.log(error);
+      // console.log(error);
+      logger("error", error, scriptName);
     }
   });
-  console.log(`${folderNameArray.length} unique folder names found`);
+  // console.log(`${folderNameArray.length} unique folder names found`);
+  await logger(
+    "info",
+    `${folderNameArray.length} unique folder names found`,
+    scriptName
+  );
 }
 
 getFolderNamesToArray();
+// console.log(folderNameArray);
 
 module.exports = folderNameArray;
