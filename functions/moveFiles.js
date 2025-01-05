773e817e7334f5798f1bd4ee2ed61c2b27d70894
niktsa <nikita-98@hotmail.com>
2025-01-05 15:45:07 +0100

    byttet til logger() og foundationArray

--- functions/moveFiles.js
+++ functions/moveFiles.js
@@ -1,34 +1,31 @@
 var fs = require("fs");
 require("dotenv").config({ path: "../.env" });
 const new_file_path = process.env.new_file_path;
-const original_file_path = process.env.original_file_path;
+const logger = require("../helpers/logger");
+var path = require("path");
+var scriptName = path.basename(__filename);
 const { foundationArray, files } = require("../functions/foundation");
 const isFile = require("./isFile");
 let count = 0;
 
 async function moveFiles() {
-  files.forEach((e) => {
+  foundationArray.forEach((e) => {
     try {
       //if its a file, move
-      if (isFile(e)) {
-        const fileArrayWithName = e.split("/");
-        // if the array has more then 1 indexes, chose the last
-        const nameOffile =
-          fileArrayWithName[fileArrayWithName.length - 1] ||
-          fileArrayWithName[0];
-        // console.log(nameOffile);
-
-        const stats = fs.statSync(`${original_file_path}/${e}`);
+      if (isFile(`${e.path}/${e.fileName}`)) {
+        const stats = fs.statSync(`${e.path}/${e.fileName}`);
         let modifiedDate = stats.mtime;
         modifiedDate = modifiedDate.toISOString().split("T");
         let formatedDate = modifiedDate[0];
 
         // console.log(`${original_file_path}/${e}`);
         fs.rename(
-          `${original_file_path}/${e}`,
-          `${new_file_path}/${formatedDate}/${nameOffile}`,
+          `${e.path}/${e.fileName}`,
+          `${new_file_path}/${formatedDate}/${e.fileName}`,
           (err) => {
-            if (err) throw err;
+            if (err) {
+              logger("error", err, scriptName);
+            }
             // console.log(`Renamed ${original_file_path}/${e}: ${original_file_path}/${formatedDate}/${nameOffile}`);
           }
         );
@@ -38,10 +35,12 @@ async function moveFiles() {
         // console.log(e);
       }
     } catch (error) {
-      console.log(error);
+      // console.log(error);
+      logger("error", error, scriptName);
     }
   });
-  console.log(`Moved ${count} files`);
+  // console.log(`Moved ${count} files`);
+  logger("info", `Moved ${count} files`, scriptName);
 }
 
 module.exports = moveFiles;
