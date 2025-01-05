ad10330f7d96de849c08dc7dc381bbf248998791
niktsa <nikita-98@hotmail.com>
2025-01-05 15:43:33 +0100

    byttet til logger(), endra til foundationArray

--- functions/copyFiles.js
+++ functions/copyFiles.js
@@ -1,39 +1,39 @@
 var fs = require("fs");
 require("dotenv").config({ path: "../.env" });
+const logger = require("../helpers/logger");
+var path = require("path");
+var scriptName = path.basename(__filename);
 const new_file_path = process.env.new_file_path;
-const original_file_path = process.env.original_file_path;
-const { foundationArray, files } = require("../functions/foundation");
+const { foundationArray, files } = require("./foundation");
 let count = 0;
 
 async function copyFiles() {
-  files.forEach((e) => {
-    const fileArrayWithName = e.split("/");
-    // if the array has more then 1 indexes, chose the last
-    const nameOffile =
-      fileArrayWithName[fileArrayWithName.length - 1] || fileArrayWithName[0];
-    // console.log(nameOffile);
-
-    const stats = fs.statSync(`${original_file_path}/${e}`);
+  foundationArray.forEach((e) => {
+    const stats = fs.statSync(`${e.path}/${e.fileName}`);
     let modifiedDate = stats.mtime;
     modifiedDate = modifiedDate.toISOString().split("T");
     let formatedDate = modifiedDate[0];
     try {
       fs.copyFile(
-        `${original_file_path}/${e}`,
-        `${new_file_path}/${formatedDate}/${nameOffile}`,
+        `${e.path}/${e.fileName}`,
+        `${new_file_path}/${formatedDate}/${e.fileName}`,
         (err) => {
           if (err) {
-            console.log(err);
-            console.log(`${original_file_path}/${e}`);
+            // console.log(err);
+            // console.log(`${e.filePath}/${e.fileName}`);
+            logger("error", err, scriptName);
+            logger("error", `${e.filePath}/${e.fileName}`, scriptName);
           }
         }
       );
       count++;
     } catch (error) {
-      console.log(error);
+      // console.log(error);
+      logger("error", error, scriptName);
     }
   });
-  console.log(`Copied ${count} files`);
+  // console.log(`Copied ${count} files`);
+  await logger("info", `Copied ${count} files`, scriptName);
 }
 
 module.exports = copyFiles;
