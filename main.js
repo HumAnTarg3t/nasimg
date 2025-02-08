d8308ccd974630a9d8a06b0355e93cbbb90b94b3
niktsa <nikita-98@hotmail.com>
2025-02-08 14:25:34 +0100

    renamed some functions

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const startCreateFolders = require("./functions/createFolders");
-const copyFiles = require("./functions/copyFiles");
-const moveFiles = require("./functions/moveFiles");
+// const copyFiles = require("./functions/copyFiles");
+const startCopyFiles = require("./functions/copyFiles");
+const startMoveFiles = require("./functions/moveFiles");
 
 async function start() {
   // await getFolderNamesToArray();
   await startCreateFolders();
   // await copyFiles();
-  await moveFiles();
+  // await startCopyFiles()
+  await startMoveFiles();
 }
 start();
